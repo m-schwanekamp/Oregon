a4c19817df5a4a064c25aa6c81571e78ee43ad3d
NathanRasmussen <112090840+NathanRasmussen@users.noreply.github.com>
2023-03-30 18:25:35 -0400

    Update event.java

--- app/src/main/java/com/example/oregon/event.java
+++ app/src/main/java/com/example/oregon/event.java
@@ -1,9 +1,12 @@
+import java.util.Random;
+
 public class event {
     private String name;
     private int id;
     private int people = 0;
     private boolean activity = false;
     private int chance;
+    private Random random = new Random();
 
     public event (String nam, int chan, int i)
     {
@@ -32,9 +35,20 @@ public class event {
         return activity;
     }
 
-    public void activate ()
+    public void activate (Location loc, player play)
     {
-        activity = true;
+        int n = random.nextInt() % 100;
+        
+        if (n <= chance) {
+            switch (id) {
+                case 0:
+                    dysentaryCon();
+                    break;
+                case 1:
+                    dysentaryDie(play);
+                    break;
+            }
+        }
     }
 
     public void deactivate ()
@@ -50,5 +64,16 @@ public class event {
     public void setChance (int x)
     {
         chance = x;
+    }
+    
+    public void dysentaryCon ()
+    {
+        activity = true;
+        people ++;
+    }
+    
+    public void dysentaryDie (player play)
+    {
+        play.death();
     }
 }
